b316f0125ed5c23bec27d74eed450bc3abec651c
Tran Hoang Phuc <PhucTHCE182086@fpt.edu.vn>
2026-02-28 10:24:09 +0700

    L1: add model vao map, them level 1 cleaning room

---
+++ Assets/L1_Home/Scripts/Cleaning/L1H_CleanupEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class L1H_CleanupEvents
+{
+    // total, cleaned
+    public static Action<int, int> OnProgressChanged;
+
+    // remaining seconds
+    public static Action<float> OnTimeChanged;
+
+    public static Action OnWin;
+    public static Action OnLose;
+}
